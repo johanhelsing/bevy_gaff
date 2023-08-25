15d9c2abe49195f7216df3ea192275861a2ec5de
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2023-08-25 13:52:35 +0200

    Almost working

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use args::*;
 use bevy::core::{Pod, Zeroable};
+use bevy::ecs::schedule::ScheduleLabel;
 use bevy::log::LogPlugin;
 use bevy::{diagnostic::FrameTimeDiagnosticsPlugin, prelude::*, sprite::MaterialMesh2dBundle};
-use bevy_ggrs::ggrs::{Config, PlayerHandle, SessionBuilder};
-use bevy_ggrs::{GgrsPlugin, GgrsSchedule, PlayerInputs, Session};
+use bevy_ggrs::ggrs::{Config, PlayerHandle, PlayerType, SessionBuilder};
+use bevy_ggrs::{
+    AddRollbackCommandExtension, GgrsAppExtension, GgrsPlugin, GgrsSchedule, PlayerInputs, Session,
+};
 use bevy_matchbox::prelude::*;
 use bevy_xpbd_2d::{math::*, prelude::*};
 use grabber_2d::GrabberPlugin;
@@ -28,21 +31,27 @@ pub struct BoxInput {
     pub inp: u8,
 }
 
-// fn main() {
-//     App::new()
-//         .add_systems(Update, pause_button)
-//         .add_systems(Update, movement)
-//         .run();
-// }
-
 #[derive(Component)]
 struct Marble;
 
-fn setup_scene(
-    mut commands: Commands,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-    mut meshes: ResMut<Assets<Mesh>>,
-) {
+#[derive(Component, Default, Reflect)]
+#[reflect(Component, Hash)]
+struct PreviousPosition(Vec2);
+
+impl std::hash::Hash for PreviousPosition {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.0.x.to_bits().hash(state);
+        self.0.y.to_bits().hash(state);
+    }
+}
+
+#[derive(Resource, Default, Reflect, Hash, Deref, DerefMut)]
+#[reflect(Resource, Hash)]
+pub struct FrameCount {
+    pub frame: usize,
+}
+
+fn setup_scene(mut commands: Commands) {
     let square_sprite = Sprite {
         color: Color::rgb(0.7, 0.7, 0.8),
         custom_size: Some(Vec2::splat(50.0)),
@@ -93,6 +102,18 @@ fn setup_scene(
         Position(Vector::X * 50.0 * 9.5),
         Collider::cuboid(50.0, 50.0 * 11.0),
     ));
+}
+
+fn spawn_marbles(
+    mut commands: Commands,
+    frame_count: Res<FrameCount>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+) {
+    if **frame_count != 0 {
+        return;
+    }
+    info!("Spawning marbles");
 
     let marble_radius = 10.0;
     let marble_mesh = MaterialMesh2dBundle {
@@ -103,22 +124,27 @@ fn setup_scene(
         ..default()
     };
 
+    let half_width = 3;
+    let half_height = 3;
     // Spawn stacks of marbles
-    for x in -10..10 {
-        for y in -10..10 {
+    for x in -half_width..half_width {
+        for y in -half_height..half_height {
             let position = Vector::new(
                 x as Scalar * (2.5 * marble_radius),
                 y as Scalar * (2.5 * marble_radius),
             );
-            commands.spawn((
-                marble_mesh.clone(),
-                RigidBody::Dynamic,
-                Position(position),
-                Rotation::default(),
-                Collider::ball(marble_radius),
-                Friction::new(0.0),
-                Marble,
-            ));
+            commands
+                .spawn((
+                    marble_mesh.clone(),
+                    RigidBody::Dynamic,
+                    Position(position),
+                    Rotation::default(),
+                    Collider::ball(marble_radius),
+                    Friction::new(0.0),
+                    PreviousPosition(position),
+                    Marble,
+                ))
+                .add_rollback();
         }
     }
 }
@@ -146,32 +172,33 @@ fn movement(
     }
 }
 
-fn pause_button(
-    current_state: ResMut<State<AppState>>,
-    mut next_state: ResMut<NextState<AppState>>,
-    keys: Res<Input<KeyCode>>,
-) {
-    if keys.just_pressed(KeyCode::P) {
-        let new_state = match current_state.get() {
-            AppState::Paused => AppState::InGame,
-            AppState::InGame => AppState::Paused,
-            _ => current_state.clone(),
-        };
-        next_state.0 = Some(new_state);
-    }
-}
+// fn pause_button(
+//     current_state: ResMut<State<AppState>>,
+//     mut next_state: ResMut<NextState<AppState>>,
+//     keys: Res<Input<KeyCode>>,
+// ) {
+//     if keys.just_pressed(KeyCode::P) {
+//         let new_state = match current_state.get() {
+//             AppState::Paused => AppState::InGame,
+//             AppState::InGame => AppState::Paused,
+//             _ => current_state.clone(),
+//         };
+//         next_state.0 = Some(new_state);
+//     }
+// }
 
-fn step_button(mut physics_loop: ResMut<PhysicsLoop>, keys: Res<Input<KeyCode>>) {
-    if keys.just_pressed(KeyCode::Return) {
-        physics_loop.step();
-    }
-}
+// fn step_button(mut physics_loop: ResMut<PhysicsLoop>, keys: Res<Input<KeyCode>>) {
+//     if keys.just_pressed(KeyCode::Return) {
+//         physics_loop.step();
+//     }
+// }
 
 const FPS: usize = 60;
 
 #[derive(Debug, Clone, Default, Eq, PartialEq, Hash, States)]
 enum AppState {
     #[default]
+    Startup,
     Lobby,
     InGame,
     Paused,
@@ -179,68 +206,99 @@ enum AppState {
 
 const SKY_COLOR: Color = Color::rgb(0.69, 0.69, 0.69);
 
+#[derive(ScheduleLabel, Clone, Debug, Hash, Eq, PartialEq)]
+struct PhysicsSchedule;
+
 fn main() {
     // read query string or command line arguments
     let args = Args::get();
     info!("{args:?}");
 
-    let mut app = App::new();
-
-    GgrsPlugin::<GgrsConfig>::new()
-        // define frequency of rollback game logic update
-        .with_update_frequency(FPS)
-        // define system that returns inputs given a player handle, so GGRS can send the inputs
-        // around
-        .with_input_system(input)
-        // register types of components AND resources you want to be rolled back
-        .register_rollback_component::<Transform>()
-        // .register_rollback_component::<Velocity>()
-        // make it happen in the bevy app
-        .build(&mut app);
-
-    app.add_plugins((
-        DefaultPlugins
-            .set(LogPlugin {
-                filter: "info,wgpu_core=warn,wgpu_hal=warn,matchbox_socket=debug".into(),
-                level: bevy::log::Level::DEBUG,
-            })
-            .set(WindowPlugin {
-                primary_window: Some(Window {
-                    fit_canvas_to_parent: true, // behave on wasm
+    App::new()
+        .add_plugins((
+            DefaultPlugins
+                .set(LogPlugin {
+                    filter: "info,wgpu_core=warn,wgpu_hal=warn,matchbox_socket=debug".into(),
+                    level: bevy::log::Level::DEBUG,
+                })
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        fit_canvas_to_parent: true, // behave on wasm
+                        ..default()
+                    }),
                     ..default()
                 }),
-                ..default()
-            }),
-        PhysicsPlugins::default(),
-        FrameTimeDiagnosticsPlugin,
-        GrabberPlugin,
-    ))
-    .insert_resource(ClearColor(SKY_COLOR))
-    .insert_resource(ClearColor(Color::rgb(0.05, 0.05, 0.1)))
-    .insert_resource(SubstepCount(6))
-    .insert_resource(Gravity(Vector::NEG_Y * 1000.0))
-    // Some of our systems need the query parameters
-    .insert_resource(args)
-    .add_state::<AppState>()
-    .add_systems(Startup, setup)
-    .add_systems(
-        OnEnter(AppState::Lobby),
-        (lobby_startup, start_matchbox_socket),
-    )
-    .add_systems(Update, lobby_system.run_if(in_state(AppState::Lobby)))
-    .add_systems(OnExit(AppState::Lobby), lobby_cleanup)
-    .add_systems(OnEnter(AppState::InGame), setup_scene)
-    .add_systems(Update, log_ggrs_events.run_if(in_state(AppState::InGame)))
-    // these systems will be executed as part of the advance frame update
-    .add_systems(GgrsSchedule, movement)
-    // .add_systems(OnEnter(AppState::Paused), bevy_xpbd_2d::pause)
-    // .add_systems(OnExit(AppState::Paused), bevy_xpbd_2d::resume)
-    // .add_systems(Update, step_button.run_if(in_state(AppState::Paused)))
-    .run();
+            PhysicsPlugins::new(PhysicsSchedule),
+            FrameTimeDiagnosticsPlugin,
+            GrabberPlugin,
+        ))
+        .add_ggrs_plugin(
+            GgrsPlugin::<GgrsConfig>::new()
+                // define frequency of rollback game logic update
+                .with_update_frequency(FPS)
+                // define system that returns inputs given a player handle, so GGRS can send the inputs
+                // around
+                .with_input_system(input) // register types of components AND resources you want to be rolled back
+                .register_rollback_component::<Transform>()
+                .register_rollback_component::<Position>()
+                .register_rollback_component::<LinearVelocity>()
+                .register_rollback_component::<AngularVelocity>()
+                .register_rollback_component::<PreviousPosition>()
+                .register_rollback_resource::<FrameCount>(),
+        )
+        .insert_resource(ClearColor(SKY_COLOR))
+        .insert_resource(ClearColor(Color::rgb(0.05, 0.05, 0.1)))
+        .insert_resource(SubstepCount(6))
+        .insert_resource(Gravity(Vector::NEG_Y * 1000.0))
+        .insert_resource(PhysicsTimestep::FixedOnce(1. / 60.))
+        .init_resource::<FrameCount>()
+        // Some of our systems need the query parameters
+        .insert_resource(args)
+        .add_state::<AppState>()
+        .add_systems(Startup, setup)
+        .add_systems(
+            OnEnter(AppState::Lobby),
+            (lobby_startup, start_matchbox_socket),
+        )
+        .add_systems(Update, lobby_system.run_if(in_state(AppState::Lobby)))
+        .add_systems(OnExit(AppState::Lobby), lobby_cleanup)
+        .add_systems(OnEnter(AppState::InGame), setup_scene)
+        .add_systems(Update, log_ggrs_events.run_if(in_state(AppState::InGame)))
+        // these systems will be executed as part of the advance frame update
+        .add_systems(
+            GgrsSchedule,
+            (
+                spawn_marbles,
+                movement,
+                step_physics,
+                update_previous_position,
+                increase_frame_system,
+            )
+                .chain(),
+        )
+        // .add_systems(OnEnter(AppState::Paused), bevy_xpbd_2d::pause)
+        // .add_systems(OnExit(AppState::Paused), bevy_xpbd_2d::resume)
+        // .add_systems(Update, step_button.run_if(in_state(AppState::Paused)))
+        .run();
 }
 
-fn setup(mut commands: Commands) {
+fn setup(mut commands: Commands, mut app_state: ResMut<NextState<AppState>>, args: Res<Args>) {
     commands.spawn(Camera2dBundle::default());
+    if args.players == 1 {
+        info!("starting synctest session");
+        let mut session_builder = configure_session(1);
+        session_builder = session_builder
+            .add_player(PlayerType::Local, 0)
+            .expect("failed to add player");
+        let session = session_builder
+            .start_synctest_session()
+            .expect("failed to start synctest session");
+        commands.insert_resource(Session::SyncTest(session));
+        app_state.set(AppState::InGame)
+    } else {
+        info!("joining multiplayer lobby");
+        app_state.set(AppState::Lobby)
+    }
 }
 
 fn start_matchbox_socket(mut commands: Commands, args: Res<Args>) {
@@ -300,8 +358,8 @@ fn lobby_startup(mut commands: Commands, asset_server: Res<AssetServer>) {
 }
 
 fn lobby_cleanup(query: Query<Entity, With<LobbyUI>>, mut commands: Commands) {
-    for e in query.iter() {
-        commands.entity(e).despawn_recursive();
+    for entity in query.iter() {
+        commands.entity(entity).despawn_recursive();
     }
 }
 
@@ -333,18 +391,10 @@ fn lobby_system(
     // extract final player list
     let players = socket.players();
 
-    let max_prediction = 12;
-
-    // create a GGRS P2P session
-    let mut sess_build = SessionBuilder::<GgrsConfig>::new()
-        .with_num_players(args.players)
-        .with_max_prediction_window(max_prediction)
-        .with_input_delay(2)
-        .with_fps(FPS)
-        .expect("invalid fps");
+    let mut session_builder = configure_session(args.players);
 
     for (i, player) in players.into_iter().enumerate() {
-        sess_build = sess_build
+        session_builder = session_builder
             .add_player(player, i)
             .expect("failed to add player");
     }
@@ -352,14 +402,23 @@ fn lobby_system(
     let channel = socket.take_channel(0).unwrap();
 
     // start the GGRS session
-    let sess = sess_build
+    let session = session_builder
         .start_p2p_session(channel)
         .expect("failed to start session");
 
-    commands.insert_resource(Session::P2P(sess));
+    commands.insert_resource(Session::P2P(session));
 
     // transition to in-game state
     app_state.set(AppState::InGame);
+}
+
+fn configure_session(players: usize) -> SessionBuilder<GgrsConfig> {
+    SessionBuilder::<GgrsConfig>::new()
+        .with_num_players(players)
+        .with_max_prediction_window(12)
+        .with_input_delay(2)
+        .with_fps(FPS)
+        .expect("invalid fps")
 }
 
 fn log_ggrs_events(mut session: ResMut<Session<GgrsConfig>>) {
@@ -369,7 +428,8 @@ fn log_ggrs_events(mut session: ResMut<Session<GgrsConfig>>) {
                 info!("GGRS Event: {event:?}");
             }
         }
-        _ => panic!("This example focuses on p2p."),
+        Session::SyncTest(_) => {}
+        _ => panic!("This example focuses on p2p and synctest"),
     }
 }
 
@@ -395,4 +455,18 @@ fn input(_handle: In<PlayerHandle>, keyboard_input: Res<Input<KeyCode>>) -> BoxI
     }
 
     BoxInput { inp: input }
+}
+
+fn increase_frame_system(mut frame_count: ResMut<FrameCount>) {
+    frame_count.frame += 1;
+}
+
+fn update_previous_position(mut positions: Query<(&mut PreviousPosition, &Position)>) {
+    for (mut previous_position, position) in &mut positions {
+        previous_position.0 = position.0;
+    }
+}
+
+fn step_physics(world: &mut World) {
+    world.run_schedule(PhysicsSchedule);
 }
