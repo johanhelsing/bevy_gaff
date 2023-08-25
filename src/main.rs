2e8794a764354024b71bf59a55db4e9de8b08f6d
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2023-08-25 12:41:13 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,398 @@
+use args::*;
+use bevy::core::{Pod, Zeroable};
+use bevy::log::LogPlugin;
+use bevy::{diagnostic::FrameTimeDiagnosticsPlugin, prelude::*, sprite::MaterialMesh2dBundle};
+use bevy_ggrs::ggrs::{Config, PlayerHandle, SessionBuilder};
+use bevy_ggrs::{GgrsPlugin, GgrsSchedule, PlayerInputs, Session};
+use bevy_matchbox::prelude::*;
+use bevy_xpbd_2d::{math::*, prelude::*};
+use grabber_2d::GrabberPlugin;
+
+mod args;
+mod grabber_2d;
+
+/// You need to define a config struct to bundle all the generics of GGRS. You can safely ignore
+/// `State` and leave it as u8 for all GGRS functionality.
+/// TODO: Find a way to hide the state type.
+#[derive(Debug)]
+pub struct GgrsConfig;
+impl Config for GgrsConfig {
+    type Input = BoxInput;
+    type State = u8;
+    type Address = PeerId;
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, PartialEq, Eq, Pod, Zeroable)]
+pub struct BoxInput {
+    pub inp: u8,
+}
+
+// fn main() {
+//     App::new()
+//         .add_systems(Update, pause_button)
+//         .add_systems(Update, movement)
+//         .run();
+// }
+
+#[derive(Component)]
+struct Marble;
+
+fn setup_scene(
+    mut commands: Commands,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+) {
+    let square_sprite = Sprite {
+        color: Color::rgb(0.7, 0.7, 0.8),
+        custom_size: Some(Vec2::splat(50.0)),
+        ..default()
+    };
+
+    // Ceiling
+    commands.spawn((
+        SpriteBundle {
+            sprite: square_sprite.clone(),
+            transform: Transform::from_scale(Vec3::new(20.0, 1.0, 1.0)),
+            ..default()
+        },
+        RigidBody::Static,
+        Position(Vector::Y * 50.0 * 6.0),
+        Collider::cuboid(50.0 * 20.0, 50.0),
+    ));
+    // Floor
+    commands.spawn((
+        SpriteBundle {
+            sprite: square_sprite.clone(),
+            transform: Transform::from_scale(Vec3::new(20.0, 1.0, 1.0)),
+            ..default()
+        },
+        RigidBody::Static,
+        Position(Vector::NEG_Y * 50.0 * 6.0),
+        Collider::cuboid(50.0 * 20.0, 50.0),
+    ));
+    // Left wall
+    commands.spawn((
+        SpriteBundle {
+            sprite: square_sprite.clone(),
+            transform: Transform::from_scale(Vec3::new(1.0, 11.0, 1.0)),
+            ..default()
+        },
+        RigidBody::Static,
+        Position(Vector::NEG_X * 50.0 * 9.5),
+        Collider::cuboid(50.0, 50.0 * 11.0),
+    ));
+    // Right wall
+    commands.spawn((
+        SpriteBundle {
+            sprite: square_sprite,
+            transform: Transform::from_scale(Vec3::new(1.0, 11.0, 1.0)),
+            ..default()
+        },
+        RigidBody::Static,
+        Position(Vector::X * 50.0 * 9.5),
+        Collider::cuboid(50.0, 50.0 * 11.0),
+    ));
+
+    let marble_radius = 10.0;
+    let marble_mesh = MaterialMesh2dBundle {
+        mesh: meshes
+            .add(shape::Circle::new(marble_radius as f32).into())
+            .into(),
+        material: materials.add(ColorMaterial::from(Color::rgb(0.2, 0.7, 0.9))),
+        ..default()
+    };
+
+    // Spawn stacks of marbles
+    for x in -10..10 {
+        for y in -10..10 {
+            let position = Vector::new(
+                x as Scalar * (2.5 * marble_radius),
+                y as Scalar * (2.5 * marble_radius),
+            );
+            commands.spawn((
+                marble_mesh.clone(),
+                RigidBody::Dynamic,
+                Position(position),
+                Rotation::default(),
+                Collider::ball(marble_radius),
+                Friction::new(0.0),
+                Marble,
+            ));
+        }
+    }
+}
+
+fn movement(
+    inputs: Res<PlayerInputs<GgrsConfig>>,
+    mut marbles: Query<&mut LinearVelocity, With<Marble>>,
+) {
+    for input in inputs.iter() {
+        let input = input.0.inp;
+        for mut linear_velocity in &mut marbles {
+            if input & INPUT_UP != 0 {
+                linear_velocity.y += 50.0;
+            }
+            if input & INPUT_DOWN != 0 {
+                linear_velocity.y -= 10.0;
+            }
+            if input & INPUT_LEFT != 0 {
+                linear_velocity.x -= 10.0;
+            }
+            if input & INPUT_RIGHT != 0 {
+                linear_velocity.x += 10.0;
+            }
+        }
+    }
+}
+
+fn pause_button(
+    current_state: ResMut<State<AppState>>,
+    mut next_state: ResMut<NextState<AppState>>,
+    keys: Res<Input<KeyCode>>,
+) {
+    if keys.just_pressed(KeyCode::P) {
+        let new_state = match current_state.get() {
+            AppState::Paused => AppState::InGame,
+            AppState::InGame => AppState::Paused,
+            _ => current_state.clone(),
+        };
+        next_state.0 = Some(new_state);
+    }
+}
+
+fn step_button(mut physics_loop: ResMut<PhysicsLoop>, keys: Res<Input<KeyCode>>) {
+    if keys.just_pressed(KeyCode::Return) {
+        physics_loop.step();
+    }
+}
+
+const FPS: usize = 60;
+
+#[derive(Debug, Clone, Default, Eq, PartialEq, Hash, States)]
+enum AppState {
+    #[default]
+    Lobby,
+    InGame,
+    Paused,
+}
+
+const SKY_COLOR: Color = Color::rgb(0.69, 0.69, 0.69);
+
+fn main() {
+    // read query string or command line arguments
+    let args = Args::get();
+    info!("{args:?}");
+
+    let mut app = App::new();
+
+    GgrsPlugin::<GgrsConfig>::new()
+        // define frequency of rollback game logic update
+        .with_update_frequency(FPS)
+        // define system that returns inputs given a player handle, so GGRS can send the inputs
+        // around
+        .with_input_system(input)
+        // register types of components AND resources you want to be rolled back
+        .register_rollback_component::<Transform>()
+        // .register_rollback_component::<Velocity>()
+        // make it happen in the bevy app
+        .build(&mut app);
+
+    app.add_plugins((
+        DefaultPlugins
+            .set(LogPlugin {
+                filter: "info,wgpu_core=warn,wgpu_hal=warn,matchbox_socket=debug".into(),
+                level: bevy::log::Level::DEBUG,
+            })
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    fit_canvas_to_parent: true, // behave on wasm
+                    ..default()
+                }),
+                ..default()
+            }),
+        PhysicsPlugins::default(),
+        FrameTimeDiagnosticsPlugin,
+        GrabberPlugin,
+    ))
+    .insert_resource(ClearColor(SKY_COLOR))
+    .insert_resource(ClearColor(Color::rgb(0.05, 0.05, 0.1)))
+    .insert_resource(SubstepCount(6))
+    .insert_resource(Gravity(Vector::NEG_Y * 1000.0))
+    // Some of our systems need the query parameters
+    .insert_resource(args)
+    .add_state::<AppState>()
+    .add_systems(Startup, setup)
+    .add_systems(
+        OnEnter(AppState::Lobby),
+        (lobby_startup, start_matchbox_socket),
+    )
+    .add_systems(Update, lobby_system.run_if(in_state(AppState::Lobby)))
+    .add_systems(OnExit(AppState::Lobby), lobby_cleanup)
+    .add_systems(OnEnter(AppState::InGame), setup_scene)
+    .add_systems(Update, log_ggrs_events.run_if(in_state(AppState::InGame)))
+    // these systems will be executed as part of the advance frame update
+    .add_systems(GgrsSchedule, movement)
+    // .add_systems(OnEnter(AppState::Paused), bevy_xpbd_2d::pause)
+    // .add_systems(OnExit(AppState::Paused), bevy_xpbd_2d::resume)
+    // .add_systems(Update, step_button.run_if(in_state(AppState::Paused)))
+    .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
+fn start_matchbox_socket(mut commands: Commands, args: Res<Args>) {
+    let room_id = match &args.room {
+        Some(id) => id.clone(),
+        None => format!("bevy_ggrs?next={}", &args.players),
+    };
+
+    let room_url = format!("{}/{}", &args.matchbox, room_id);
+    info!("connecting to matchbox server: {room_url:?}");
+
+    commands.insert_resource(MatchboxSocket::new_ggrs(room_url));
+}
+
+// Marker components for UI
+#[derive(Component)]
+struct LobbyText;
+#[derive(Component)]
+struct LobbyUI;
+
+fn lobby_startup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // All this is just for spawning centered text.
+    commands
+        .spawn(NodeBundle {
+            style: Style {
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
+                position_type: PositionType::Absolute,
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::FlexEnd,
+                ..default()
+            },
+            background_color: Color::rgb(0.43, 0.41, 0.38).into(),
+            ..default()
+        })
+        .with_children(|parent| {
+            parent
+                .spawn(TextBundle {
+                    style: Style {
+                        align_self: AlignSelf::Center,
+                        justify_content: JustifyContent::Center,
+                        ..default()
+                    },
+                    text: Text::from_section(
+                        "Entering lobby...",
+                        TextStyle {
+                            font: asset_server.load("fonts/quicksand-light.ttf"),
+                            font_size: 96.,
+                            color: Color::BLACK,
+                        },
+                    ),
+                    ..default()
+                })
+                .insert(LobbyText);
+        })
+        .insert(LobbyUI);
+}
+
+fn lobby_cleanup(query: Query<Entity, With<LobbyUI>>, mut commands: Commands) {
+    for e in query.iter() {
+        commands.entity(e).despawn_recursive();
+    }
+}
+
+fn lobby_system(
+    mut app_state: ResMut<NextState<AppState>>,
+    args: Res<Args>,
+    mut socket: ResMut<MatchboxSocket<SingleChannel>>,
+    mut commands: Commands,
+    mut query: Query<&mut Text, With<LobbyText>>,
+) {
+    // regularly call update_peers to update the list of connected peers
+    for (peer, new_state) in socket.update_peers() {
+        // you can also handle the specific dis(connections) as they occur:
+        match new_state {
+            PeerState::Connected => info!("peer {peer} connected"),
+            PeerState::Disconnected => info!("peer {peer} disconnected"),
+        }
+    }
+
+    let connected_peers = socket.connected_peers().count();
+    let remaining = args.players - (connected_peers + 1);
+    query.single_mut().sections[0].value = format!("Waiting for {remaining} more player(s)",);
+    if remaining > 0 {
+        return;
+    }
+
+    info!("All peers have joined, going in-game");
+
+    // extract final player list
+    let players = socket.players();
+
+    let max_prediction = 12;
+
+    // create a GGRS P2P session
+    let mut sess_build = SessionBuilder::<GgrsConfig>::new()
+        .with_num_players(args.players)
+        .with_max_prediction_window(max_prediction)
+        .with_input_delay(2)
+        .with_fps(FPS)
+        .expect("invalid fps");
+
+    for (i, player) in players.into_iter().enumerate() {
+        sess_build = sess_build
+            .add_player(player, i)
+            .expect("failed to add player");
+    }
+
+    let channel = socket.take_channel(0).unwrap();
+
+    // start the GGRS session
+    let sess = sess_build
+        .start_p2p_session(channel)
+        .expect("failed to start session");
+
+    commands.insert_resource(Session::P2P(sess));
+
+    // transition to in-game state
+    app_state.set(AppState::InGame);
+}
+
+fn log_ggrs_events(mut session: ResMut<Session<GgrsConfig>>) {
+    match session.as_mut() {
+        Session::P2P(s) => {
+            for event in s.events() {
+                info!("GGRS Event: {event:?}");
+            }
+        }
+        _ => panic!("This example focuses on p2p."),
+    }
+}
+
+const INPUT_UP: u8 = 1 << 0;
+const INPUT_DOWN: u8 = 1 << 1;
+const INPUT_LEFT: u8 = 1 << 2;
+const INPUT_RIGHT: u8 = 1 << 3;
+
+fn input(_handle: In<PlayerHandle>, keyboard_input: Res<Input<KeyCode>>) -> BoxInput {
+    let mut input: u8 = 0;
+
+    if keyboard_input.pressed(KeyCode::W) {
+        input |= INPUT_UP;
+    }
+    if keyboard_input.pressed(KeyCode::A) {
+        input |= INPUT_LEFT;
+    }
+    if keyboard_input.pressed(KeyCode::S) {
+        input |= INPUT_DOWN;
+    }
+    if keyboard_input.pressed(KeyCode::D) {
+        input |= INPUT_RIGHT;
+    }
+
+    BoxInput { inp: input }
+}
